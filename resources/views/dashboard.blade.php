d55567f1afe44b815caf2ac52e48a46c3170f257
12AJ <rajingalepatil@gmail.com>
2022-10-25 12:49:16 +0530

    secondcommit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,9 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/dashboard.css">
     <title>dashboard</title>
 </head>
 <body>
+    <nav>
+
+    </nav>
     
+    <main>
+        <div class="totalAmountDiv">
+            <h2 class="totalAmountHeading">Total Amount Recived</h2>
+            <p class="totalAmount">123456 ₹</p>
+        </div>
+       
+
+    </main>
 </body>
 </html>
