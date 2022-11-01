2190d85f6c1b22a0cc2476f7f8305c8aa915843f
12AJ <rajingalepatil@gmail.com>
2022-11-01 10:01:04 +0530

    minor changes

--- resources/views/components/donation-entries.blade.php
+++ resources/views/components/donation-entries.blade.php
@@ -1,10 +1,8 @@
 <div class="detailsDiv">
 
     <p class="name">{{$num['name']}}</p>
-<div>
     <p>{{$num['address']}}</p>
     <p>{{$num['email']}}</p>
-    <p>{{$num['mobile']}}</p>
-</div>
+    <p class="mobile">{{$num['mobile']}}</p>
     <p class="amount">+ {{$num['amount']}} ₹</p>
 </div>
